03a2e033deb6d9f0da1e2ed32cdc7c1a6eecb90e
wildheart007 <cang.b.le@gmail.com>
2018-06-05 04:02:27 -0700

    header is responsive to 700px

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,53 +8,98 @@ export default class HeaderComponent extends Component {
     };
   }
 
-  headerToggle = () => {
-    //let { ariaExpanded } = this.state;
-    this.setState(prevState => ({
-      ariaExpanded: !prevState.ariaExpanded
-    }));
-  };
+  // headerToggle = () => {
+  //   //let { ariaExpanded } = this.state;
+  //   this.setState(prevState => ({
+  //     ariaExpanded: !prevState.ariaExpanded
+  //   }));
+  // };
 
   render() {
-    let { ariaExpanded } = this.state;
+    // let { ariaExpanded } = this.state;
     return (
-      <div className="wrapper-header">
-        <header>
-          <div className="header-inner">
-            <span id="logo">
-              <a id="logo-link">
-                CANG <span id="lassName">LE</span>
-              </a>
-            </span>
-            <nav id="nav">
-              <ul>
-                <a class="navLink" href="#">
-                  Home
-                </a>
-                <a class="navLink" href="#">
-                  About
-                </a>
-                <a class="navLink" href="#">
-                  Contact
-                </a>
-                <a class="navLink" href="#">
-                  Linkedin
-                </a>
-                {/* <button
-                  aria-expanded={ariaExpanded}
-                  aria-controls="menu-list"
-                  onClick={this.headerToggle}>
-                  {ariaExpanded ? (
-                    <span className="close">X</span>
-                  ) : (
-                    <span className="open">☰</span>
-                  )}
-                </button> */}
-              </ul>
-            </nav>
+      <div className="header-container">
+        <div className="header-grid">
+          <div id="full-name-logo">
+            <a id="logo-link">
+              CANG <span id="lassName">LE</span>
+            </a>
           </div>
-        </header>
+          <div className="navi-grid" id="navi">
+            <a id="navi-home" className="naviLink" href="#">
+              Home
+            </a>
+            <a id="navi-about" className="naviLink" href="#">
+              About
+            </a>
+            <a id="navi-contact" className="naviLink" href="#">
+              Contact
+            </a>
+            <a id="navi-linkedIn" className="naviLink" href="#">
+              LinkedIn
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+// import React, { Component } from 'react';
+//
+// export default class HeaderComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       ariaExpanded: false
+//     };
+//   }
+//
+//   headerToggle = () => {
+//     //let { ariaExpanded } = this.state;
+//     this.setState(prevState => ({
+//       ariaExpanded: !prevState.ariaExpanded
+//     }));
+//   };
+//
+//   render() {
+//     let { ariaExpanded } = this.state;
+//     return (
+//       <div className="wrapper-header">
+//         <div className="header-container">
+//           <span id="logo">
+//             <a id="logo-link">
+//               CANG <span id="lassName">LE</span>
+//             </a>
+//           </span>
+//           <nav className="nav">
+//             <button
+//               aria-expanded={ariaExpanded}
+//               aria-controls="header-list"
+//               onClick={this.headerToggle}>
+//               {ariaExpanded ? (
+//                 <span className="close">X</span>
+//               ) : (
+//                 <span className="open">☰</span>
+//               )}
+//             </button>
+//             <ul id="header-list">
+//               <a class="navLink" href="#">
+//                 Home
+//               </a>
+//               <a class="navLink" href="#">
+//                 About
+//               </a>
+//               <a class="navLink" href="#">
+//                 Contact
+//               </a>
+//               <a class="navLink" href="#">
+//                 Linkedin
+//               </a>
+//             </ul>
+//           </nav>
+//         </div>
+//       </div>
+//     );
+//   }
+// }

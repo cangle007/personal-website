e4a64c9e8f151ab1e3ff8f6db5ffe7fbebc8256f
wildheart007 <cang.b.le@gmail.com>
2018-06-11 02:48:03 -0700

    updated banner css

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -4,7 +4,7 @@ export default function BannerComponent() {
   return (
     <div className="banner-wrapper">
       <div className="banner-container">
-        <div class="header-img-mgs">
+        <div class="banner-header">
           <h1>Hello. I am Cang</h1>
           <p>a full-developer</p>
         </div>

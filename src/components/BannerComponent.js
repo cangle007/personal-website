fa793dec3c554b4637220ec0767564fb1e69a34a
wildheart007 <cang.b.le@gmail.com>
2018-06-11 02:41:35 -0700

    update banner css

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function BannerComponent() {
   return (
-    <div className="wrapper-header-img" id="header-img-1">
-      <div className="inner">
+    <div className="banner-wrapper">
+      <div className="banner-container">
         <div class="header-img-mgs">
           <h1>Hello. I am Cang</h1>
           <p>a full-developer</p>
